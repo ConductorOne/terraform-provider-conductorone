f4d753fd896b3798cb59079948a55a6a3066fd9d
michael burton <michaelburton@michaels-MacBook-Pro.local>
2023-07-06 12:41:15 -0700

    namespace fix

--- examples/policy.tf
+++ examples/policy.tf
@@ -1,20 +1,20 @@
 terraform {
   required_providers {
-    c1 = {
+    terraform = {
       source = "registry.terraform.io/speakeasy/conductorone"
       version = "1.0.0"
     }
   }
 }
 
-provider "c1" {
-  server_url = ""
+provider "terraform" {
+  server_url = "https://mstanb.sandbox.ductone.com"
   client_id = ""
   client_secret = ""
 }
 
-resource "c1_terraform_policy" "plcy" {
-  name = "Terraform Created Policy"
+resource "terraform_policy" "plcy" {
+  display_name = "Terraform Created Policy"
   description = "This policy was created via terraform"
-
+  policy_type = "POLICY_TYPE_CERTIFY"
 }
