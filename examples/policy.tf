f1b3fd5867088656ac9908cc3622f0a4f5813873
michael burton <michaelburton@michaels-MBP.domain>
2023-07-11 13:13:46 -0700

    changes policy.tf

--- examples/policy.tf
+++ examples/policy.tf
@@ -1,19 +1,19 @@
 terraform {
   required_providers {
-    terraform = {
-      source = "registry.terraform.io/speakeasy/conductorone"
+    conductorone = {
+      source = "registry.terraform.io/ConductorOne/conductorone"
       version = "1.0.0"
     }
   }
 }
 
-provider "terraform" {
+provider "conductorone" {
   server_url = "<server_url>"
   client_id = "<client_id>"
   client_secret = "<client_secret>"
 }
 
-resource "terraform_policy" "new_policy" {
+resource "conductorone_policy" "new_policy" {
   display_name = "Terraform Created Policy"
   description = "New description"
   policy_type = "POLICY_TYPE_CERTIFY"
@@ -35,30 +35,6 @@ resource "terraform_policy" "new_policy" {
               fallback_user_ids = [
                 "2P4VnGXgZv5PDKQ9zEJnjEXNBxY"
               ]
-            }
-          }
-        }
-      ]
-    }
-  }
-}
-
-resource "terraform_policy" "prov_policy" {
-  display_name = "Terraform Created Prov Policy"
-  description = "New description"
-  policy_type = "POLICY_TYPE_GRANT"
-  reassign_tasks_to_delegates = "false"
-  policy_steps = {
-    grant = {
-      steps = [
-        {
-          provision = {
-            assigned = "false"
-            provision_policy = {
-              delegated_provision = {
-                app_id = "2P4WqESCtljFQ46vSDX9Cred22S"
-                entitlement_id = "2P4X9MMlOTlQ80l1umHhpuukkWW"
-              }
             }
           }
         }
