4f85d0de9bd7f29451e5b0d848192483e1c089aa
Justin Gallardo <justin.gallardo@conductorone.com>
2023-07-25 23:08:10 -0700

    make generate

--- internal/sdk/connector.go
+++ internal/sdk/connector.go
@@ -4,13 +4,13 @@ package sdk
 
 import (
 	"bytes"
+	"conductorone/internal/sdk/pkg/models/operations"
+	"conductorone/internal/sdk/pkg/models/shared"
+	"conductorone/internal/sdk/pkg/utils"
 	"context"
 	"fmt"
 	"io"
 	"net/http"
-	"conductorone/internal/sdk/pkg/models/operations"
-	"conductorone/internal/sdk/pkg/models/shared"
-	"conductorone/internal/sdk/pkg/utils"
 )
 
 type connector struct {
