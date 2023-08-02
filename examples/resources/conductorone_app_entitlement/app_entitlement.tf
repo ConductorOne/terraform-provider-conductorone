f3723916666f5e41ba7e57b9efc2c089eb4b57e2
michael burton <michaelburton@michaels-MBP.domain>
2023-08-02 13:50:59 -0700

    rebase

--- examples/resources/conductorone_app_entitlement/app_entitlement.tf
+++ examples/resources/conductorone_app_entitlement/app_entitlement.tf
@@ -11,4 +11,7 @@ resource "conductorone_app_entitlement" "okta_test_admin" {
     }
   }
   risk_level_value_id = conductorone_risk_level.test_risk_level.id
+  compliance_framework_value_ids = [
+    data.conductorone_compliance_framework.soc2.id
+  ]
 }
