5c557ee28567c7fa85fb37d5161c7e5f56619e62
Anthony Zhao <anthony.zhao@conductorone.com>
2023-11-06 10:33:40 -0800

    Speakeasy gen

---
+++ terraform/examples/data-sources/conductorone_policy/data-source.tf
@@ -0,0 +1,3 @@
+data "conductorone_policy" "my_policy" {
+  id = "f7f6f435-e73d-4137-b83e-44adac49d30b"
+}
---
+++ terraform/examples/provider/provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    conductorone = {
+      source  = "ConductorOne/conductorone"
+      version = "1.0.0"
+    }
+  }
+}
+
+provider "conductorone" {
+  # Configuration options
+}
