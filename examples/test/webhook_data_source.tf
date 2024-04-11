c3e6a198bdb2cc0be0abdda05a804c3a6ca36d5a
michael burton <michaelburton@michaels-MacBook-Pro.local>
2024-04-11 15:02:03 -0700

    remove accidental code

--- examples/test/webhook_data_source.tf
+++
@@ -1,3 +0,0 @@
-data "conductorone_webhook" "test_webhook" {
-  display_name = "test no https"
-}
--- examples/test/webhook_resource.tf
+++
@@ -1,5 +0,0 @@
-# resource "conductorone_webhook" "test_webhook" {
-#   display_name = "new TF sourced webhook"
-#   url          = "https://webhook-test.com"
-#   description  = "testing"
-# }
