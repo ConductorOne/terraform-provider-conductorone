c0a648a0928a23b31a1b74926bfa5339d640a82f
michael burton <michaelburton@michaels-MacBook-Pro.local>
2024-10-11 16:44:21 -0700

    Implements the app entitlement proxy binding resource

---
+++ examples/resources/conductorone_app_entitlement_proxy_binding/app_entitlement_proxy_binding.tf
@@ -0,0 +1,6 @@
+resource "conductorone_app_entitlement_proxy_binding" "test_binding" {
+  src_app_id             = "<src_app_id>"
+  src_app_entitlement_id = "<src_app_entitlement_id>"
+  dst_app_id             = "<dst_app_id>"
+  dst_app_entitlement_id = "<dst_app_entitlement_id>"
+}
