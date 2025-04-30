765ff3fdbd417e2ddb8c0555a0c28e4587556a1d
Alan Lee <lee.alan.c12@gmail.com>
2025-04-30 16:30:02 -0700

    rm test files.

--- examples/test/appentitlements.tf
+++
@@ -1,9 +0,0 @@
-data "conductorone_app_entitlements" "tests" {
-  app_ids = [
-    "2w97z0DmRXQKy39hUOEbi5Bc3gP"
-  ]
-}
-
-output "tests" {
-  value = data.conductorone_app_entitlements.tests.list
-}
