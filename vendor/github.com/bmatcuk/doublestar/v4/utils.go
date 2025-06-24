2ea4fd051641f7eaae2f836c9c9db5f279903dde
Luisina Santos <luisina.santos@conductorone.com>
2025-06-24 18:08:35 -0300

    make generate

--- vendor/github.com/bmatcuk/doublestar/v4/utils.go
+++ vendor/github.com/bmatcuk/doublestar/v4/utils.go
@@ -14,16 +14,16 @@ import (
 // The second string is everything after that slash. For example, given the
 // pattern:
 //
-//   ../../path/to/meta*/**
-//                ^----------- split here
+//	../../path/to/meta*/**
+//	             ^----------- split here
 //
 // SplitPattern returns "../../path/to" and "meta*/**". This is useful for
 // initializing os.DirFS() to call Glob() because Glob() will silently fail if
 // your pattern includes `/./` or `/../`. For example:
 //
-//   base, pattern := SplitPattern("../../path/to/meta*/**")
-//   fsys := os.DirFS(base)
-//   matches, err := Glob(fsys, pattern)
+//	base, pattern := SplitPattern("../../path/to/meta*/**")
+//	fsys := os.DirFS(base)
+//	matches, err := Glob(fsys, pattern)
 //
 // If SplitPattern cannot find somewhere to split the pattern (for example,
 // `meta*/**`), it will return "." and the unaltered pattern (`meta*/**` in
@@ -32,7 +32,6 @@ import (
 // Of course, it is your responsibility to decide if the returned base path is
 // "safe" in the context of your application. Perhaps you could use Match() to
 // validate against a list of approved base directories?
-//
 func SplitPattern(p string) (base, pattern string) {
 	base = "."
 	pattern = p
@@ -82,7 +81,6 @@ func SplitPattern(p string) (base, pattern string) {
 //
 // Note: the returned error doublestar.ErrBadPattern is not equal to
 // filepath.ErrBadPattern.
-//
 func FilepathGlob(pattern string, opts ...GlobOption) (matches []string, err error) {
 	if pattern == "" {
 		// special case to match filepath.Glob behavior
