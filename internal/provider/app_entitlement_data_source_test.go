024359bf65dd29e09f30c3f4fbc6b1ebc8572b35
subencheng <ben.su@conductorone.com>
2025-08-06 23:22:29 -0700

    add test

--- internal/provider/app_entitlement_data_source_test.go
+++ internal/provider/app_entitlement_data_source_test.go
@@ -10,52 +10,52 @@ func TestAccAppEntitlementDataSource(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		ProtoV6ProviderFactories: testAccProviderFactories,
 		Steps: []resource.TestStep{
-			// Create an app with a custom entitlement to query
-			// {
-			// 	Config: providerConfig + `
-			// 	resource "conductorone_app" "test" {
-			// 		display_name = "test-app-entitlement-ds"
-			// 		description = "test app for entitlement data source"
-			// 	}
+			Create an app with a custom entitlement to query
+			{
+				Config: providerConfig + `
+				resource "conductorone_app" "test" {
+					display_name = "test-app-entitlement-ds"
+					description = "test app for entitlement data source"
+				}
 
-			// 	resource "conductorone_app_resource_type" "test" {
-			// 		display_name  = "GROUP"
-			// 		app_id       = conductorone_app.test.id
-			// 		resource_type = "GROUP"
-			// 	}
+				resource "conductorone_app_resource_type" "test" {
+					display_name  = "GROUP"
+					app_id       = conductorone_app.test.id
+					resource_type = "GROUP"
+				}
 
-			// 	resource "conductorone_app_resource" "test" {
-			// 		app_id = conductorone_app.test.id
-			// 		display_name = "test-resource"
-			// 		description = "test resource for entitlement data source"
-			// 		app_resource_type_id = conductorone_app_resource_type.test.id
-			// 	}
+				resource "conductorone_app_resource" "test" {
+					app_id = conductorone_app.test.id
+					display_name = "test-resource"
+					description = "test resource for entitlement data source"
+					app_resource_type_id = conductorone_app_resource_type.test.id
+				}
 
-			// 	resource "conductorone_risk_level" "high" {
-			// 		value = "Extra high"
-			// 	}
+				resource "conductorone_risk_level" "high" {
+					value = "Extra high"
+				}
 
-			// 	resource "conductorone_compliance_framework" "soc2" {
-			// 		value = "SOC 2"
-			// 	}
+				resource "conductorone_compliance_framework" "soc2" {
+					value = "SOC 2"
+				}
 
-			// 	resource "conductorone_custom_app_entitlement" "test" {
-			// 		app_id               = conductorone_app.test.id
-			// 		app_resource_id      = conductorone_app_resource.test.id
-			// 		app_resource_type_id = conductorone_app_resource_type.test.id
-			// 		display_name         = "test-entitlement"
-			// 		description         = "test entitlement for data source"
-			// 		alias               = "test-alias"
-			// 		provision_policy = {
-			// 			connector_provision = {}
-			// 		}
-			// 		risk_level_value_id = resource.conductorone_risk_level.high.id
-			// 		compliance_framework_value_ids = [resource.conductorone_compliance_framework.soc2.id]
-			// 		purpose = "APP_ENTITLEMENT_PURPOSE_VALUE_ASSIGNMENT"
-			// 		duration_unset = {}
-			// 	}
-			// 	`,
-			// },
+				resource "conductorone_custom_app_entitlement" "test" {
+					app_id               = conductorone_app.test.id
+					app_resource_id      = conductorone_app_resource.test.id
+					app_resource_type_id = conductorone_app_resource_type.test.id
+					display_name         = "test-entitlement"
+					description         = "test entitlement for data source"
+					alias               = "test-alias"
+					provision_policy = {
+						connector_provision = {}
+					}
+					risk_level_value_id = resource.conductorone_risk_level.high.id
+					compliance_framework_value_ids = [resource.conductorone_compliance_framework.soc2.id]
+					purpose = "APP_ENTITLEMENT_PURPOSE_VALUE_ASSIGNMENT"
+					duration_unset = {}
+				}
+				`,
+			},
 			// Test fetching the entitlement by alias
 			{
 				Config: providerConfig + `
@@ -102,6 +102,7 @@ func TestAccAppEntitlementDataSource(t *testing.T) {
 				}
 
 				data "conductorone_app_entitlement" "by_alias" {
+					app_ids = [conductorone_app.test.id]
 					alias = conductorone_custom_app_entitlement.test.alias
 				}
 				`,
@@ -110,7 +111,6 @@ func TestAccAppEntitlementDataSource(t *testing.T) {
 					resource.TestCheckResourceAttr("data.conductorone_app_entitlement.by_alias", "description", "test entitlement for data source"),
 					resource.TestCheckResourceAttr("data.conductorone_app_entitlement.by_alias", "alias", "test-alias"),
 					resource.TestCheckResourceAttrSet("data.conductorone_app_entitlement.by_alias", "id"),
-					resource.TestCheckResourceAttrPair("data.conductorone_app_entitlement.by_alias", "app_id", "conductorone_app.test", "id"),
 					resource.TestCheckResourceAttrPair("data.conductorone_app_entitlement.by_alias", "app_resource_id", "conductorone_app_resource.test", "id"),
 					resource.TestCheckResourceAttrPair("data.conductorone_app_entitlement.by_alias", "app_resource_type_id", "conductorone_app_resource_type.test", "id"),
 				),
