00786b8623d0dafff1ff31521df50e4b76fd2fc1
subencheng <ben.su@conductorone.com>
2025-08-06 21:26:14 -0700

    test

--- internal/provider/app_entitlement_data_source_test.go
+++ internal/provider/app_entitlement_data_source_test.go
@@ -11,51 +11,51 @@ func TestAccAppEntitlementDataSource(t *testing.T) {
 		ProtoV6ProviderFactories: testAccProviderFactories,
 		Steps: []resource.TestStep{
 			// Create an app with a custom entitlement to query
-			{
-				Config: providerConfig + `
-				resource "conductorone_app" "test" {
-					display_name = "test-app-entitlement-ds"
-					description = "test app for entitlement data source"
-				}
+			// {
+			// 	Config: providerConfig + `
+			// 	resource "conductorone_app" "test" {
+			// 		display_name = "test-app-entitlement-ds"
+			// 		description = "test app for entitlement data source"
+			// 	}
 
-				resource "conductorone_app_resource_type" "test" {
-					display_name  = "GROUP"
-					app_id       = conductorone_app.test.id
-					resource_type = "GROUP"
-				}
+			// 	resource "conductorone_app_resource_type" "test" {
+			// 		display_name  = "GROUP"
+			// 		app_id       = conductorone_app.test.id
+			// 		resource_type = "GROUP"
+			// 	}
 
-				resource "conductorone_app_resource" "test" {
-					app_id = conductorone_app.test.id
-					display_name = "test-resource"
-					description = "test resource for entitlement data source"
-					app_resource_type_id = conductorone_app_resource_type.test.id
-				}
+			// 	resource "conductorone_app_resource" "test" {
+			// 		app_id = conductorone_app.test.id
+			// 		display_name = "test-resource"
+			// 		description = "test resource for entitlement data source"
+			// 		app_resource_type_id = conductorone_app_resource_type.test.id
+			// 	}
 
-				resource "conductorone_risk_level" "high" {
-					value = "Extra high"
-				}
+			// 	resource "conductorone_risk_level" "high" {
+			// 		value = "Extra high"
+			// 	}
 
-				resource "conductorone_compliance_framework" "soc2" {
-					value = "SOC 2"
-				}
+			// 	resource "conductorone_compliance_framework" "soc2" {
+			// 		value = "SOC 2"
+			// 	}
 
-				resource "conductorone_custom_app_entitlement" "test" {
-					app_id               = conductorone_app.test.id
-					app_resource_id      = conductorone_app_resource.test.id
-					app_resource_type_id = conductorone_app_resource_type.test.id
-					display_name         = "test-entitlement"
-					description         = "test entitlement for data source"
-					alias               = "test-alias"
-					provision_policy = {
-						connector_provision = {}
-					}
-					risk_level_value_id = resource.conductorone_risk_level.high.id
-					compliance_framework_value_ids = [resource.conductorone_compliance_framework.soc2.id]
-					purpose = "APP_ENTITLEMENT_PURPOSE_VALUE_ASSIGNMENT"
-					duration_unset = {}
-				}
-				`,
-			},
+			// 	resource "conductorone_custom_app_entitlement" "test" {
+			// 		app_id               = conductorone_app.test.id
+			// 		app_resource_id      = conductorone_app_resource.test.id
+			// 		app_resource_type_id = conductorone_app_resource_type.test.id
+			// 		display_name         = "test-entitlement"
+			// 		description         = "test entitlement for data source"
+			// 		alias               = "test-alias"
+			// 		provision_policy = {
+			// 			connector_provision = {}
+			// 		}
+			// 		risk_level_value_id = resource.conductorone_risk_level.high.id
+			// 		compliance_framework_value_ids = [resource.conductorone_compliance_framework.soc2.id]
+			// 		purpose = "APP_ENTITLEMENT_PURPOSE_VALUE_ASSIGNMENT"
+			// 		duration_unset = {}
+			// 	}
+			// 	`,
+			// },
 			// Test fetching the entitlement by alias
 			{
 				Config: providerConfig + `
