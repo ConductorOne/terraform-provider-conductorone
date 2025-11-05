8faf89c65e8b386bdc743677e0a996698b95a332
rch <robert.chiniquy@conductorone.com>
2025-11-05 14:59:19 -0800

    make generate

--- examples/resources/conductorone_app_entitlement_owner/import-by-string-id.tf
+++ examples/resources/conductorone_app_entitlement_owner/import-by-string-id.tf
@@ -1,7 +1,7 @@
 import {
   to = conductorone_app_entitlement_owner.my_conductorone_app_entitlement_owner
   id = jsonencode({
-    app_id = "..."
+    app_id         = "..."
     entitlement_id = "..."
   })
 }
